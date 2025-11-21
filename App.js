5f35a60102b24a8e17cc1a198059b6cfad8d7449
csrijon <csrijon92@gmail.com>
2025-11-21 13:43:42 +0530

    new project

---
+++ App.js
@@ -0,0 +1,14 @@
+// import React, { useState, useEffect } from "react";
+import { View, Text, } from "react-native";
+
+const App = () => {
+ 
+
+  return (
+    <View>
+      <Text style={{color:"white"}} >hello i am a guy</Text>
+    </View>
+  );
+};
+
+export default App;
