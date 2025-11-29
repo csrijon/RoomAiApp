0eb000f7ccdd6f229666237ec5b834a71496e7e2
csrijon <csrijon92@gmail.com>
2025-11-29 17:52:35 +0530

    push

--- src/screen/ReviewPage.js
+++ src/screen/ReviewPage.js
@@ -1,16 +1,22 @@
 import { SafeAreaView } from "react-native-safe-area-context"
 import { View, Image, Text, StyleSheet } from "react-native"
+import ExtractedDetails from "../components/ExtractedDetails.js"
 
 const ReviewPage = () => {
     return (
         <SafeAreaView style={styles.reviewsection}  >
             <View style={styles.reviewheader} >
-                <Image source={require("../images/logoweb.png")} style={{ width: 60, height: 60 }} />
+                <Image source={require("../images/logoweb.png")} style={{ width: 70, height: 70 }} />
                 <View>
-                    <Text>Room AI Designer</Text>
+                    <Text style={styles.boldtext} >Room AI Designer</Text>
                     <Text>Statements By Srijonchowdhury</Text>
                 </View>
             </View>
+            <Text style={styles.reviewtext} >Review Your Space</Text>
+
+            <Image style={styles.image} source={require("../images/roomview.png")} />
+
+            <ExtractedDetails />
         </SafeAreaView>
     )
 
@@ -28,5 +34,21 @@ const styles = StyleSheet.create({
     reviewheader: {
         flexDirection: "row",
         alignItems: "center"
+    },
+    boldtext: {
+        fontSize: 17,
+        fontWeight: 600
+    },
+    reviewtext: {
+        fontSize: 17,
+        fontWeight: 500,
+        marginTop: 20,
+        marginLeft: 4
+    },
+    image: {
+        width: 350,
+        height: 300,
+        resizeMode: "center",
+        alignSelf: "center"
     }
 })
