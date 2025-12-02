e2c509b93b051c0ca7ecc10f3e9369e060ec071a
csrijon <csrijon92@gmail.com>
2025-12-02 23:03:54 +0530

    push

--- src/screen/ParttwoScaner.js
+++ src/screen/ParttwoScaner.js
@@ -1,13 +1,23 @@
 import SecendHeader from "../components/SecendHeader.js"
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet,View } from "react-native";
+import { Text } from "react-native/types_generated/index";
+import { Color } from "react-native/types_generated/Libraries/Animated/AnimatedExports";
 
 const ParttwoScaner =()=>{
     return(
-     <SafeAreaView  >
+     <SafeAreaView style={styles.PartScaner} >
         <SecendHeader/>
+        <View><Text style={{Color:"black"}} >Living Room Project</Text></View>
      </SafeAreaView>
     )
 }
 
 export default ParttwoScaner
 
+const styles =StyleSheet.create({
+    PartScaner:{
+        flex:1,
+        backgroundColor:"#ffffff"
+    }
+})
