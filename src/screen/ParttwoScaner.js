aca82a88c49c1b6eb91a9ba415991447d6f17fcf
csrijon <csrijon92@gmail.com>
2025-12-02 22:51:50 +0530

    push

---
+++ src/screen/ParttwoScaner.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ParttwoScaner =()=>{
+    return(
+     <SafeAreaView>
+        <Header/>
+     </SafeAreaView>
+    )
+}
+
+export default ParttwoScaner
