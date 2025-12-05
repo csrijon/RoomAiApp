7f9bcbdbad77257d3c347b4012eee8066d7c547b
Srijon Chowdhury <csrijon92@gmail.com>
2025-12-05 12:39:42 +0530

    push

--- App.js
+++ App.js
@@ -1,30 +1,33 @@
 // import React, { useState, useEffect } from "react";
 // import { View, Text, } from "react-native";
 // import Roomsection from "./src/screen/Roomsection.js"
-// import HomePage from "./src/screen/HomePage.js"
+import HomePage from "./src/screen/HomePage.js"
 // import HelpSupportPage from "./src/screen/HelpSupportPage.js"
 // import Subscrption from "./src/screen/Subscrption.js"
 // import Roomsence from "./src/screen/Roomsence"
 // import Tellmeus from "./src/screen/Tellmeus"
-import ReviewPage from "./src/screen/ReviewPage.js"
+// import ReviewPage from "./src/screen/ReviewPage.js"
 // import ParttwoScaner from "./src/screen/ParttwoScaner.js"
 // import ScanerLoader from "./src/screen/ScanerLoadingmainpage.js"
 // import Mainscanerpage from "./src/screen/MainScanerpage.js"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 
+const Stacksscreen = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }} >
+      <Stack.Screen name="Home" component={HomePage} />
+    </Stack.Navigator>
+  )
+}
 const App = () => {
 
   return (
-    //  <Roomsection/>
-    // <HomePage />
-    // <HelpSupportPage/>
-    // <Subscrption/>
-    // <Roomsence/>
-    // <Tellmeus/>
-    <ReviewPage />
-    // <ParttwoScaner/>
-    // <ScanerLoader/>
-    // <Mainscanerpage/>
+    <NavigationContainer>
+      <Stacksscreen/>
+    </NavigationContainer>
   );
 };
 
