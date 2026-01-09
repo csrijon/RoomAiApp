115dce3fbed1ccf44f6d54fd41b63b0dc244b7bf
Srijon Chowdhury <csrijon92@gmail.com>
2026-01-09 14:10:48 +0530

    push

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
+
+
 module.exports = {
   root: true,
-  extends: '@react-native',
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-  },
+  extends: '@react-native/eslint-config',
+  requireConfigFile: false,
 };
